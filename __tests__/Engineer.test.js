40372257b1dbb3afcf9930bc19a6a4b6a0808716
RKH1988 <regettaspearman@gmail.com>
2022-10-16 22:30:34 -0400

    added Intern tests and update to Engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,6 +6,7 @@ test('checks for all employee inherited properties',()=>{
     expect (engineer.name).toEqual(expect.any(String));
     expect (engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
 });
 
 //getGithub and getRole (Engineer) tests
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,22 @@
 const Intern = require('../lib/Intern');
 
-//getSchool and getRole (intern)
+
+//test to ensure Intern has necessary properties
+test('checks for all employee inherited properties',()=>{
+    const intern = new Intern('namex',3,'myemailx@company.com','UNC');
+    expect (intern.name).toEqual(expect.any(String));
+    expect (intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+//getSchool and getRole (intern) tests
+test('checks that intern GitHub was created',()=>{
+    const intern = new Intern('namex',3,'myemailx@company.com','UNC');
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+test('checks that role is equal to Intern',()=>{
+    const intern = new Intern('namex',3,'myemailx@company.com','UNC');
+    expect(intern.getRole()).toBe('Intern');
+});
