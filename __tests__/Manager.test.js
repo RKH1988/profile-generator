4e573b381c29f6df29fd383b8ee397771e129e77
RKH1988 <regettaspearman@gmail.com>
2022-10-16 22:35:42 -0400

    created Manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,15 @@
 const Manager = require('../lib/Manager');
 
-//getRole (manager)
+//test to ensure Intern has necessary properties
+test('checks for all employee inherited properties',()=>{
+    const manager = new Manager('namew',4,'myemailw@company.com','125');
+    expect (manager.name).toEqual(expect.any(String));
+    expect (manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('checks that role is equal to Manager',()=>{
+    const manager = new Manager('namew',4,'myemailw@company.com','125');
+    expect(manager.getRole()).toBe('Manager');
+});
